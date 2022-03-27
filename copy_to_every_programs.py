783c30d95f6305dfe1941d6d7fdf836f0633b0e3
FreedomSX <544986329@qq.com>
2022-03-27 17:34:29 +0800

    n7

---
+++ copy_to_every_programs.py
@@ -0,0 +1,18 @@
+import os
+
+# 获取项目文件夹中所有项目文件夹列表
+folder_lst = [i for i in os.listdir(".\\") if os.path.isdir(os.path.join(".\\", i)) and (not i.startswith("Environment"))]
+# print(folder_lst)
+
+py_file_lst = [i for i in os.listdir(".\\") if os.path.isfile(os.path.join(".\\", i)) and i.endswith(".py") and (not i.startswith("copy_to_every_programs"))]
+
+for folder in folder_lst:
+    print(folder.find("Environment"))
+    if folder.find("Environment") == -1:
+        cmdStr = "xcopy .\\Environment .\\" + folder + "\\Environment /d /y /i /h /e"
+        # print(cmdStr)
+        os.system(cmdStr)
+        for file in py_file_lst:
+            cmdStr = "copy .\\" + file + " .\\" + folder + "\\" + file + " /y"
+            # print(cmdStr)
+            os.system(cmdStr)
